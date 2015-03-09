d3b46bd121943aa2270a59f9d76c9eb531fb15e7
mafernando <fernando_michael@bah.com>
2015-03-09 10:00:40 -0500

    Updated version and added additional default hooks.

--- config/initializers/subscriptions.rb
+++ config/initializers/subscriptions.rb
@@ -1,16 +1,14 @@
-# TODO: ADD THIS NOTIFICATIONS IN APPROPRIATELY
-# ActiveSupport::Notifications.subscribe('sessions#create/pre_hook') do |*args|
-#   JellyfishNotice.send(*args)
-# end
+ActiveSupport::Notifications.subscribe('sessions#create/pre_hook') do |*args|
+  JellyfishNotice.send(*args)
+end
 
 ActiveSupport::Notifications.subscribe('sessions#create/post_hook') do |*args|
   JellyfishNotice.send(*args)
 end
 
-# TODO: ADD THIS NOTIFICATIONS IN APPROPRIATELY
-# ActiveSupport::Notifications.subscribe('sessions#destroy/pre_hook') do |*args|
-#   JellyfishNotice.send(*args)
-# end
+ActiveSupport::Notifications.subscribe('sessions#destroy/pre_hook') do |*args|
+  JellyfishNotice.send(*args)
+end
 
 ActiveSupport::Notifications.subscribe('sessions#destroy/post_hook') do |*args|
   JellyfishNotice.send(*args)
